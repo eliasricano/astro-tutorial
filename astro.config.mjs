63682396acc5236a57a422b167cdabd45a32c11d
eliasricano <eliasricano@gmail.com>
2024-11-24 00:46:00 -0600

    added rss feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://resilient-horse-c2ac8a.netlify.app/"
+});
